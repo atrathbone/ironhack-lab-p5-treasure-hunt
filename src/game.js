a5c37eecc72bf5feab8cdd87415e0052f872667b
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-04-01 16:16:32 +0200

    create grid

--- src/game.js
+++ src/game.js
@@ -1,7 +1,14 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    rect(0, 0, 500, 500);
+    stroke('red');
+    strokeWeight(4);
+    fill(0, 255, 0)
+    for (let i = 0; i < 9; i++) {
+      let n = i*50+50;
+      line(0, n, 500, n);
+      line(n, 0, n, 500);
+    }
+
   }
 }
